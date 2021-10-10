08def0478ed4b018a7b19fca4b9a5a3f8db0c292
Snake9889 <vya4eslaw.denisov@yandex.ru>
2021-10-10 21:44:41 +0700

    All done!

--- datasources_all.py
+++ datasources_all.py
@@ -106,7 +106,8 @@ class BPMDataAll(BPMTemplate):
         else:
             self.hash = [0, 0, 0, 0]
             self.statusWidget.status_2.setToolTip("Lengths of arrays from BPM-s are different")
-            self.statusWidget.status_2.setText(u'<span style="font-size: 50pt; color: red;">•</span>')
+            self.statusWidget.status_2.setStyleSheet("background-color: red; border: 1px solid black; border-radius: 10px;")
+            #self.statusWidget.status_2.setText(u'<span style="font-size: 50pt; color: red;">•</span>')
             pass
 
     def start_type_check(self):
@@ -121,11 +122,13 @@ class BPMDataAll(BPMTemplate):
         if self.hash == [0, 0, 0, 0]:
             self.no_data(self.statusWidget.status_1)
             self.statusWidget.status_4.setToolTip("No connection to server")
-            self.statusWidget.status_4.setText(u'<span style="font-size: 50pt; color: red;">•</span>')
+            self.statusWidget.status_4.setStyleSheet("background-color: red; border: 1px solid black; border-radius: 10px;")
+            #self.statusWidget.status_4.setText(u'<span style="font-size: 50pt; color: red;">•</span>')
 
         else:
             self.statusWidget.status_1.setToolTip("Some of BPM's send data too frequently or send nothing")
-            self.statusWidget.status_1.setText(u'<span style="font-size: 50pt; color: red;">•</span>')
+            self.statusWidget.status_1.setStyleSheet("background-color: red; border: 1px solid black; border-radius: 10px;")
+            #self.statusWidget.status_1.setText(u'<span style="font-size: 50pt; color: red;">•</span>')
             self.everyting_ok(self.statusWidget.status_4)
         self.hash = [0, 0, 0, 0]
         self.l = [0, 0, 0, 0]
@@ -170,13 +173,15 @@ class BPMDataAll(BPMTemplate):
     def everyting_ok(self, label):
         """   """
         label.setToolTip("Everything alright")
-        label.setText(u'<span style="font-size: 50pt; color: green;">•</span>')
+        label.setStyleSheet("background-color: green; border: 1px solid black; border-radius: 10px;")
+        #label.setText(u'<span style="font-size: 50pt; color: green;">•</span>')
 
     def no_data(self, label):
         """   """
         label.setToolTip("Data didn't come")
         #label.setStyleSheet("color : black")
-        label.setText(u'<span style="font-size: 50pt; color: blue;">•</span>')
+        #label.setText(u'<span style="font-size: 50pt; color: blue;">•</span>')
+        label.setStyleSheet("background-color: blue; border: 1px solid black; border-radius: 10px;")
 
     def read_settings(self):
         """   """
