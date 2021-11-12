84bf3c25fdaaae3664d6353768414498a8b40cdf
Snake9889 <vya4eslaw.denisov@yandex.ru>
2021-11-12 22:13:54 +0700

    Nothing changed.

--- phasewidget.py
+++ phasewidget.py
@@ -1,4 +1,6 @@
 import sys
+from pyqtgraph.Qt import QtGui, QtCore
+import pyqtgraph as pg
 from PyQt5.QtWidgets import QApplication, QWidget, QLabel
 from PyQt5.QtCore import QSize, QCoreApplication
 from PyQt5.QtGui import QPixmap, QIcon
@@ -11,17 +13,26 @@ class PhaseWidget(QWidget):
         super().__init__()
         self.initUI()
         
+    def initUI(self):
+        self.setMinimumSize(300,300)
+        self.setGeometry(300, 300, 600, 500)
+        self.setWindowTitle('Phase')
+
+        #win = pg.GraphicsWindow(title="Basic plotting examples")
+        pg.setConfigOptions(antialias=True)
+        pw = pg.PlotWidget()
+        
+        #p2 = win.addPlot(title="Multiple curves")
+        
+        #self.data_curve_1 = PlotWidget()
+            #self.show()
         icon_path = os.path.dirname(os.path.abspath(__file__))
         help_icon = QIcon()
         help_icon.addFile(os.path.join(icon_path, 'etc/icons/Psi.png'), QSize(32, 32))
         self.setWindowIcon(help_icon)
-        
-    def initUI(self):
-            self.setGeometry(300, 300, 300, 200)
-            self.setWindowTitle('Phase')
-            self.show()
 
-        
+    def calculations(self):
+        pass
     
 
         # pixmap = QPixmap(file_name)
